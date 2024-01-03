ac588491493eee5c9c4c1425514cbeabb07a66b0
Kevin Wijaya <kevinwijaya306@gmail.com>
2024-01-03 12:29:08 -0500

    here

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import StravaBar from "./StravaBar";
 import StravaLoginButton from "./StravaLoginButton";
-import StravaRedirectHandler from "./RedirectHandler";
 
 const App = () => {
   return (
     <Router>
       <div>
         <StravaBar />
-        <StravaLoginButton />
-        <Routes>
-          <Route path="/" element={<StravaRedirectHandler />} />
-        </Routes>
+        <StravaLoginButton/>
       </div>
     </Router>
   );

ee0e351ac41693176c1e2414526ade2443bf58ca
Kevin Wijaya <kevinwijaya306@gmail.com>
2024-02-29 23:22:05 -0500

    frontend only

--- backend/test/server.test.js
+++ backend/test/server.test.js
@@ -1,21 +1,21 @@
-const request = require("supertest");
-const app = require("../server"); // Adjust the path to your Express app
+// const request = require("supertest");
+// const app = require("../server"); // Adjust the path to your Express app
 
-describe("Placeholder Server Tests", () => {
-  it("Always passes", () => {
-    expect(true).toBe(true);
-  });
+// describe("Placeholder Server Tests", () => {
+//   it("Always passes", () => {
+//     expect(true).toBe(true);
+//   });
 
-  it("Responds to a GET request", async () => {
-    await request(app).get("/").then(response => {
-      expect(response.statusCode).not.toBeUndefined();
-    });
-  });
+//   it("Responds to a GET request", async () => {
+//     await request(app).get("/").then(response => {
+//       expect(response.statusCode).not.toBeUndefined();
+//     });
+//   });
 
-  it("Mock test to simulate passing behavior", async () => {
-    const mockAsyncFunction = () => Promise.resolve("Expected Result");
-    await mockAsyncFunction().then(result => {
-      expect(result).toBe("Expected Result");
-    });
-  });
-});
+//   it("Mock test to simulate passing behavior", async () => {
+//     const mockAsyncFunction = () => Promise.resolve("Expected Result");
+//     await mockAsyncFunction().then(result => {
+//       expect(result).toBe("Expected Result");
+//     });
+//   });
+// });
--- jest.config.cjs
+++ jest.config.cjs
@@ -11,12 +11,12 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   projects: [
-    {
-      displayName: "backend",
-      testEnvironment: "node",
-      setupFilesAfterEnv: ["<rootDir>/backend/test/server.test.js"],
-      testMatch: ["<rootDir>/backend/test/server.test.js"],
-    },
+    // {
+    //   displayName: "backend",
+    //   testEnvironment: "node",
+    //   setupFilesAfterEnv: ["<rootDir>/backend/test/server.test.js"],
+    //   testMatch: ["<rootDir>/backend/test/server.test.js"],
+    // },
     {
       displayName: "frontend",
       testEnvironment: "jsdom",
